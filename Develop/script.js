42fa7e2199e265de796d9e8eeceb68f91f7ac973
ggamb <glenngambardella372@gmail.com>
2021-09-07 18:14:55 -0400

    added bootstrap image

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,6 @@ var dayEl = document.getElementById("currentDay");
 function currentDate() {
     var today = moment().format("MM/DD/YYYY");
 
-    console.log(today);
-
     dayEl.textContent = today;
 };
 
